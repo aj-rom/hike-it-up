27c7140f3632d2544727d1be7dcbe80fe4587365
A.J. Romaniello <ajiellodev@gmail.com>
2021-02-23 16:58:27 -0700

    Update for heroku

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@ threads threads_count, threads_count
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+environment ENV['RACK_ENV'] || 'production'
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
