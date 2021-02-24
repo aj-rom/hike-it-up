c7f50d886915ad38011b2f82f788121ff4ad8a01
A.J. Romaniello <ajiellodev@gmail.com>
2021-02-24 12:29:07 -0700

    Create default CRUD based routes for Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,4 +9,29 @@ class ReviewsController < ApplicationController
       redirect "/login"
     end
   end
+
+  get '/reviews/:id' do
+    @review = Review.find(params[:id])
+    if @review
+      erb :'/reviews/show.html'
+    else
+      erb :'/reviews/404.html'
+    end
+  end
+
+  get '/reviews/:id/edit' do
+    @review = Review.find(params[:id])
+    if !@review || @review.user != current_user
+      redirect "/reviews"
+    else
+      erb :'reviews/edit.html'
+    end
+  end
+
+  post '/reviews/:id' do
+    @review = Review.find(params[:id])
+    @review.update(params[:review])
+
+    redirect '/reviews/:id'
+  end
 end
